0cf970cd15fcd213599dfec03c7cb41782b9987a
artemave <artemave@gmail.com>
2024-03-05 11:40:11 +0100

    Rm silly feature

--- bin/ars.mjs
+++ bin/ars.mjs
@@ -2,9 +2,7 @@
 
 import { spawn } from 'child_process'
 import path from 'path'
-import clipboardy from 'clipboardy'
 import parseArgv from '../lib/parseArgv.js'
-import retry from '../lib/retry.js'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -17,7 +15,6 @@ if (['--help', '-h'].some(o => node_args.includes(o))) {
   console.info('')
   console.info('Options:')
   console.info(' --only="bananas"                         only run test named "bananas"')
-  console.info(" --skip-debug-url-copy-to-clipboard       don't copy debug url to clipboard (when run with --inspect-brk)")
   process.exit()
 }
 
@@ -44,22 +41,6 @@ proc.on('exit', (code, signal) => {
     }
   })
 })
-
-if (child_args.includes('--inspect-brk') && !ars_args.includes('--skip-debug-url-copy-to-clipboard')) {
-  await retry(async () => {
-    const response = await fetch('http://localhost:9229/json/list')
-    const [inspectorInfo] = await response.json()
-    const debuggerUrl = inspectorInfo.devtoolsFrontendUrl.replace(/^chrome-/, '')
-
-    try {
-      await clipboardy.write(debuggerUrl)
-    } catch (e) {
-      console.error(e)
-      process.exit(1)
-    }
-    console.info(`Debug url copied to clipboard: ${debuggerUrl}`)
-  })
-}
 
 process.on('SIGINT', () => {
   proc.kill('SIGINT')

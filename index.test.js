883652587d6a6e16570308fa1329dc7e849e87a1
artemave <artemave@gmail.com>
2024-03-08 12:46:53 +0100

    Make sure parallel tests run once

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+import glob from 'fast-glob'
 import assert from 'node:assert'
 import path from 'node:path'
 import { describe, it } from 'node:test'
@@ -6,22 +7,22 @@ import { execSync } from 'node:child_process'
 describe('running tests', function() {
   it('works with bare node', function() {
     const stdout = execSync('node ./test/cli/someTest.js', { encoding: 'utf-8' })
-    assert(stdout.match('✓ 1/0'))
+    assert.match(stdout, new RegExp('✓ 1/0'))
   })
 
   it('works via cli using relative path', function() {
     const stdout = execSync('./bin/ars ./test/cli/someTest.js', { encoding: 'utf-8' })
-    assert(stdout.match('✓ 1/0'))
+    assert.match(stdout, new RegExp('✓ 1/0'))
   })
 
   it('works via cli using absolute path', function() {
     const stdout = execSync(`./bin/ars ${path.resolve('./test/cli/someTest.js')}`, { encoding: 'utf-8' })
-    assert(stdout.match('✓ 1/0'))
+    assert.match(stdout, new RegExp('✓ 1/0'))
   })
 
   it('works via cli using short path', function() {
     const stdout = execSync('./bin/ars test/cli/someTest.js', { encoding: 'utf-8' })
-    assert(stdout.match('✓ 1/0'))
+    assert.match(stdout, new RegExp('✓ 1/0'))
   })
 
   it('takes a glob for list of files', function() {
@@ -46,5 +47,12 @@ describe('running tests', function() {
   it('accepts line number', function() {
     const stdout = execSync('./bin/ars test/cli/one/otherTest.js:9', { encoding: 'utf-8' })
     assert.match(stdout, new RegExp('✓ 1/0'))
+  })
+
+  it('runs parallel tests only once', async function() {
+    execSync('./bin/ars "./test/cli/**/*Test.js"', { encoding: 'utf-8' })
+    // assert that there is only one file that matches `someTest.*`
+    const files = glob.sync('./test/tmp/someTest*')
+    assert.equal(files.length, 1)
   })
 })
--- test/cli/someTest.js
+++ test/cli/someTest.js
@@ -1,7 +1,14 @@
+import fs from 'node:fs'
 import assert from 'node:assert'
-import { it, run } from '../../lib/api.js'
+import { it, beforeEach, run } from '../../lib/api.js'
+
+beforeEach(function() {
+  fs.rmSync('./test/tmp', { recursive: true, force: true })
+  fs.mkdirSync('./test/tmp', { recursive: true })
+})
 
 it('works', function() {
+  fs.writeFileSync(`./test/tmp/someTest${Math.random()}`, 'balls')
   assert(true)
 })
 
